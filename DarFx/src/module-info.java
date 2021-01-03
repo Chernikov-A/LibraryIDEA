a84e1a89631f2705e44f189166f4a9469c7c7c2c
fandofull <62818879+fandofull@users.noreply.github.com>
2021-01-03 17:53:39 +0700

    Add files via upload
    updated design

---
+++ DarFx/src/module-info.java
@@ -0,0 +1,10 @@
+module DarFx {
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.graphics;
+    requires java.sql;
+    requires javafx.swing;
+    requires javafx.base;
+    requires jfoenix;
+    opens sample;
+}
