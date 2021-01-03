b39228afbe07f4abca75ee15e53e7fc9c281e653
fandofull <62818879+fandofull@users.noreply.github.com>
2021-01-03 17:48:35 +0700

    delete

--- DarFx/src/sample/LibraryIDEA/DarFx/src/sample/Configs.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-public class Configs {
-    protected String dbHost = "127.0.0.1";
-    protected String dbPort = "3306";
-    protected String dbUser = "Frank";
-    protected String dbPass = "id2202639";
-    protected String dbName = "library";
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# LibraryIDEA
-test project
